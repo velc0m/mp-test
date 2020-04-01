a3869acad33422fe4c0a6e26315a842cddd42710
Vitali Litvinenka <velc0m@tut.by>
2020-04-01 13:25:01 +0300

    Initial commit

---
+++ src/main/java/us/vitali/test/TestRestApplication.java
@@ -0,0 +1,11 @@
+package us.vitali.test;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class TestRestApplication extends Application {
+}
